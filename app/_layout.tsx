cbf1d5a61b683e67c9814422edb542e94206b7aa
Yato <abdellahanca3369@gmail.com>
2026-04-11 21:08:27 +0100

    feat: register review route as fullScreenModal

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,29 +7,33 @@ export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <ThemeProvider>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="viewer"
-          options={{ presentation: 'fullScreenModal', headerShown: false }}
-        />
-        <Stack.Screen
-          name="settings/scan-settings"
-          options={{ title: 'Scan Settings', presentation: 'card' }}
-        />
-        <Stack.Screen
-          name="settings/document-settings"
-          options={{ title: 'Document Settings', presentation: 'card' }}
-        />
-        <Stack.Screen
-          name="settings/printer"
-          options={{ title: 'My Printer', presentation: 'card' }}
-        />
-        <Stack.Screen
-          name="settings/about"
-          options={{ title: 'About', presentation: 'card' }}
-        />
-      </Stack>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="viewer"
+            options={{ presentation: 'fullScreenModal', headerShown: false }}
+          />
+          <Stack.Screen
+            name="review"
+            options={{ presentation: 'fullScreenModal', headerShown: false }}
+          />
+          <Stack.Screen
+            name="settings/scan-settings"
+            options={{ title: 'Scan Settings', presentation: 'card' }}
+          />
+          <Stack.Screen
+            name="settings/document-settings"
+            options={{ title: 'Document Settings', presentation: 'card' }}
+          />
+          <Stack.Screen
+            name="settings/printer"
+            options={{ title: 'My Printer', presentation: 'card' }}
+          />
+          <Stack.Screen
+            name="settings/about"
+            options={{ title: 'About', presentation: 'card' }}
+          />
+        </Stack>
       </ThemeProvider>
     </GestureHandlerRootView>
   );
